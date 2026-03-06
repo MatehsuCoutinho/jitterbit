545fa1adeff7e1c2e69012038cdc6db2d8ef8ccb
MatehsuCoutinho <matheuscoutinho.0420@gmail.com>
2026-03-06 17:12:39 -0300

    docs: add complete documentation via Swagger

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+const swaggerUi = require("swagger-ui-express")
+const YAML = require("yamljs")
+
 const authMiddleware = require("./middlewares/auth.middleware")
 const orderRoutes = require("./routes/order.routes")
 const authRoutes = require("./routes/auth.routes")
 
 const app = express()
+
+// Carrega e serve a documentação Swagger
+const swaggerDocument = YAML.load("./docs/swagger.yaml") 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(cors())
 app.use(express.json())
@@ -17,4 +24,5 @@ const PORT = 3000
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
+    console.log(`Docs available at http://localhost:${PORT}/docs`)
 })

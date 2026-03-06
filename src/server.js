5ea8ffc731ce159b1b5e8410992e51bb9ea52e9d
MatehsuCoutinho <matheuscoutinho.0420@gmail.com>
2026-03-06 14:06:09 -0300

    feat: implement jwt authentication middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+const authMiddleware = require("./middlewares/auth.middleware")
 
 //const orderRoutes = require("./routes/orderRoutes")
 const authRoutes = require("./routes/auth.routes")
@@ -12,6 +13,12 @@ app.use(express.json())
 
 //app.use("/order", orderRoutes)
 app.use("/auth", authRoutes)
+app.get("/protected", authMiddleware, (req, res) => {
+    res.json({
+        message: "Access granted",
+        user: req.user
+    })
+})
 
 const PORT = 3000
 

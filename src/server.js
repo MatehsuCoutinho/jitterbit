30a0d97c8ce505d8437c2133fcceae3f1ffe1608
MatehsuCoutinho <matheuscoutinho.0420@gmail.com>
2026-03-06 13:29:14 -0300

    chore: initialize node project with express and base structure

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+
+const orderRoutes = require("./routes/orderRoutes")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/order", orderRoutes)
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
